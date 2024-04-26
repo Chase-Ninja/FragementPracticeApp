16e6c649e2d079b96085a8d8efbeb859c21f652e
aarav <aarav.goel109@gmail.com>
2024-04-26 16:14:13 +0530

    first scommit(screen is blank, even after removing gragment view

---
+++ app/src/main/java/com/example/fragmentation/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.fragmentation
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import com.example.fragmentation.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
